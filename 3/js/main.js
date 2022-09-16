fa8c368a087b52b6f02bc18bf32e0fd875a3fbfa
Keks <keks@htmlacademy.ru>
2022-09-16 09:37:31 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/main.js
@@ -0,0 +1,18 @@
+function calculateRandomNumber(min, max) {
+  if (min < 0 || min >= max) {
+    return 'Invalid arguments';
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+calculateRandomNumber(1, 10);
+
+function сhecksLengthOfString(verifiableString, maxLenght) {
+  verifiableString = String(verifiableString);
+  if (verifiableString.length < maxLenght) {
+    return true;
+  }
+  return false;
+}
+
+сhecksLengthOfString('qwerty', 30);
