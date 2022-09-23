c22610bebef2844f81361addfb5c3a8ed3f4152a
Keks <keks@htmlacademy.ru>
2022-09-23 15:09:43 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,18 +1,12 @@
 function calculateRandomNumber(min, max) {
   if (min < 0 || min >= max) {
-    return 'Invalid arguments';
+    throw { name: 'Invalid arguments', message: 'Invalid arguments' };
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 calculateRandomNumber(1, 10);
 
-function сhecksLengthOfString(verifiableString, maxLenght) {
-  verifiableString = String(verifiableString);
-  if (verifiableString.length < maxLenght) {
-    return true;
-  }
-  return false;
-}
+const сhecksLengthOfString = (verifiableString, maxLenght) => verifiableString.length < maxLenght;
 
 сhecksLengthOfString('qwerty', 30);
