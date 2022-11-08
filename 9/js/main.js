b26d0958e9d8a0e8998b083a73a3186b7c559c28
Keks <keks@htmlacademy.ru>
2022-11-08 15:36:25 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,6 @@
+import './util.js';
+/* eslint-disable no-unused-vars */
+import { resultArray } from './data.js';
+import { showBigPicture } from './big-pictures-render.js';
+import { photosContainer } from './render.js';
+
---
+++ 9/js/render.js
@@ -0,0 +1,26 @@
+import { resultArray } from './data.js';
+import { showBigPicture } from './big-pictures-render.js';
+
+const pictureTemplate = document.querySelector('#picture').content;
+const newPictureTemplate = pictureTemplate.querySelector('.picture');
+const photosContainer = document.querySelector('.pictures');
+
+const getPhotoElement = function (photoObject) {
+  const clonedPhoto = newPictureTemplate.cloneNode(true);
+  clonedPhoto.querySelector('.picture__img').src = photoObject.url;
+  clonedPhoto.querySelector('.picture__likes').textContent = photoObject.likes;
+  clonedPhoto.querySelector('.picture__comments').textContent = photoObject.comments.length;
+  clonedPhoto.addEventListener('click', () => showBigPicture(photoObject));
+  return clonedPhoto;
+};
+
+const renderPhotos = function (photos) {
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < photos.length; i++) {
+    fragment.appendChild(getPhotoElement(photos[i]));
+  }
+  photosContainer.appendChild(fragment);
+};
+
+renderPhotos(resultArray);
+export { photosContainer };
